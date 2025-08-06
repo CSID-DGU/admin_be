2346fdbba130d26ab88fa3767fd2fa7d90bcfdcf
saokiritoni <soeun.love.lee@gmail.com>
2025-08-06 20:15:51 +0900

    [fix] #56 빠진 커밋(UserGroupRepository)을 추가한다.

--- src/main/java/DGU_AI_LAB/admin_be/domain/users/repository/UserGroupRepository.java
+++ src/main/java/DGU_AI_LAB/admin_be/domain/users/repository/UserGroupRepository.java
@@ -1,19 +1,27 @@
 package DGU_AI_LAB.admin_be.domain.users.repository;
 
+import DGU_AI_LAB.admin_be.domain.users.entity.UsedId;
 import DGU_AI_LAB.admin_be.domain.users.entity.UserGroup;
-import DGU_AI_LAB.admin_be.domain.users.entity.UserGroupId;
+import jakarta.persistence.LockModeType;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Lock;
 import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 import java.util.Optional;
 
-public interface UserGroupRepository extends JpaRepository<UserGroup, UserGroupId> {
-
+public interface UserGroupRepository extends JpaRepository<UserGroup, Long> {
     Optional<UserGroup> findByGroupName(String groupName);
+    Optional<UserGroup> findByUsedId(UsedId usedId);
 
-    @Query("SELECT u.usedId.uid FROM UserGroup u WHERE u.groupName = :groupName")
-    Optional<Long> findUidByGroupName(String groupName);
+    @Lock(LockModeType.PESSIMISTIC_WRITE)
+    @Query("SELECT MAX(u.gid) FROM UserGroup u")
+    Optional<Long> findMaxGidWithLock();
 
-    @Query("SELECT COUNT(u) > 0 FROM UserGroup u WHERE u.groupName = :groupName AND u.usedId.uid = :uid")
-    boolean existsByGroupNameAndUid(String groupName, Long uid);
+    @Query("SELECT ug.usedId.uid FROM UserGroup ug WHERE ug.groupName = :groupName")
+    Optional<Long> findUidByGroupName(@Param("groupName") String groupName);
+
+    @Query("SELECT CASE WHEN COUNT(ug) > 0 THEN true ELSE false END FROM UserGroup ug WHERE ug.groupName = :groupName AND ug.usedId.uid = :uid")
+    boolean existsByGroupNameAndUid(@Param("groupName") String groupName, @Param("uid") Long uid);
+
 }
