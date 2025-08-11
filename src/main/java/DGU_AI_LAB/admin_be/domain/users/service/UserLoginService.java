b0d26b3efd26a86e97d3ef0b91d088e3b1e78241
saokiritoni <soeun.love.lee@gmail.com>
2025-08-11 16:48:33 +0900

    [fix] #60 회원가입 시 필드 수정

--- src/main/java/DGU_AI_LAB/admin_be/domain/users/service/UserLoginService.java
+++ src/main/java/DGU_AI_LAB/admin_be/domain/users/service/UserLoginService.java
@@ -1,5 +1,7 @@
 package DGU_AI_LAB.admin_be.domain.users.service;
 
+import DGU_AI_LAB.admin_be.domain.groups.entity.Group;
+import DGU_AI_LAB.admin_be.domain.groups.repository.GroupRepository;
 import DGU_AI_LAB.admin_be.domain.users.dto.request.UserLoginRequestDTO;
 import DGU_AI_LAB.admin_be.domain.users.dto.request.UserRegisterRequestDTO;
 import DGU_AI_LAB.admin_be.domain.users.dto.response.UserTokenResponseDTO;
@@ -15,6 +17,7 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.util.concurrent.TimeUnit;
 
@@ -24,36 +27,37 @@ import java.util.concurrent.TimeUnit;
 public class UserLoginService {
 
     private final UserRepository userRepository;
+    private final GroupRepository groupRepository;
     private final PasswordEncoder passwordEncoder;
     private final JwtProvider jwtProvider;
     private final RedisTemplate<String, String> redisTemplate;
 
     private final long REFRESH_TOKEN_EXPIRE_TIME = 60 * 60 * 24 * 7;
 
+    /** 회원가입 */
+    @Transactional
     public void register(UserRegisterRequestDTO request) {
         String redisKey = "VERIFIED:" + request.email();
 
         if (!Boolean.TRUE.equals(redisTemplate.hasKey(redisKey))) {
             throw new UnauthorizedException(ErrorCode.EMAIL_NOT_VERIFIED);
         }
-
         if (userRepository.findByEmail(request.email()).isPresent()) {
             throw new BusinessException(ErrorCode.USER_ALREADY_EXISTS);
         }
 
-        User user = User.builder()
-                .email(request.email())
-                .password(passwordEncoder.encode(request.password()))
-                .name(request.name())
-                .role(Role.USER)
-                .isActive(true)
-                .build();
+        String encoded = passwordEncoder.encode(request.password());
+        User user = request.toEntity(encoded);
 
         userRepository.save(user);
+
         redisTemplate.delete(redisKey);
-        log.info("회원가입 완료 및 VERIFIED:{} 키 삭제", request.email());
+        log.info("회원가입 완료, VERIFIED:{} 키 삭제", request.email());
     }
 
+
+
+    /** 로그인 */
     public UserTokenResponseDTO login(UserLoginRequestDTO request) {
         User user = userRepository.findByEmail(request.email())
                 .orElseThrow(() -> new UnauthorizedException(ErrorCode.INVALID_LOGIN_INFO));
@@ -71,5 +75,6 @@ public class UserLoginService {
 
         return UserTokenResponseDTO.of(accessToken, refreshToken);
     }
+
 }
 
