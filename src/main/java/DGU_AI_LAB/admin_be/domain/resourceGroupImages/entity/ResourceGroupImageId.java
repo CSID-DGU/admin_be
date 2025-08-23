15489f676c8c508583cb0784f13b86f835cf02b0
kwdahun <kwdahun@naver.com>
2025-08-23 17:03:33 +0900

    [feat] #96 Creat resource group image validation table

---
+++ src/main/java/DGU_AI_LAB/admin_be/domain/resourceGroupImages/entity/ResourceGroupImageId.java
@@ -0,0 +1,24 @@
+package DGU_AI_LAB.admin_be.domain.resourceGroupImages.entity;
+
+import jakarta.persistence.Access;
+import jakarta.persistence.AccessType;
+import jakarta.persistence.Column;
+import jakarta.persistence.Embeddable;
+import lombok.*;
+
+import java.io.Serializable;
+
+@Embeddable
+@Access(AccessType.FIELD)
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
+@EqualsAndHashCode
+public class ResourceGroupImageId implements Serializable {
+
+    @Column(name = "rsgroup_id")
+    private Integer rsgroupId;
+
+    @Column(name = "image_id")
+    private Long imageId;
+}
