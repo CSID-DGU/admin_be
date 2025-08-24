8c17d5092977dc12428367a51e74d4e1ef3745f5
saokiritoni <soeun.love.lee@gmail.com>
2025-08-24 17:16:36 +0900

    [feat/fix] #100 그룹 생성 API

--- src/main/java/DGU_AI_LAB/admin_be/domain/groups/controller/GroupController.java
+++ src/main/java/DGU_AI_LAB/admin_be/domain/groups/controller/GroupController.java
@@ -1,14 +1,14 @@
 package DGU_AI_LAB.admin_be.domain.groups.controller;
 
+import DGU_AI_LAB.admin_be.domain.groups.dto.request.CreateGroupRequestDTO;
 import DGU_AI_LAB.admin_be.domain.groups.dto.response.GroupResponseDTO;
 import DGU_AI_LAB.admin_be.domain.groups.service.GroupService;
 import DGU_AI_LAB.admin_be.global.common.SuccessResponse;
+import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -23,12 +23,22 @@ public class GroupController {
     /**
      * 모든 그룹 정보 조회 API
      * GET /api/groups
-     *
      * 그룹 ID와 그룹명을 반환합니다.
      */
-    @GetMapping("")
+    @GetMapping
     public ResponseEntity<SuccessResponse<?>> getGroups() {
         List<GroupResponseDTO> groups = groupService.getAllGroups();
         return SuccessResponse.ok(groups);
+    }
+
+    /**
+     * 새로운 그룹을 생성하는 API
+     * POST /api/groups
+     */
+    @PostMapping
+    public ResponseEntity<SuccessResponse<?>> createGroup(@RequestBody @Valid CreateGroupRequestDTO dto) {
+        log.info("[createGroup] 새로운 그룹 생성 요청 접수: {}", dto.groupName());
+        GroupResponseDTO response = groupService.createGroup(dto);
+        return SuccessResponse.created(response);
     }
 }
--- src/main/java/DGU_AI_LAB/admin_be/domain/groups/entity/Group.java
+++ src/main/java/DGU_AI_LAB/admin_be/domain/groups/entity/Group.java
@@ -10,21 +10,18 @@ import lombok.*;
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
 @AllArgsConstructor
 @Builder
-@EqualsAndHashCode(of = "groupId")
+@EqualsAndHashCode(of = "ubuntuGid")
 public class Group {
 
     @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
-    @Column(name = "group_id")
-    private Long groupId;
+    @Column(name = "ubuntu_gid", nullable = false)
+    private Long ubuntuGid;
 
     @Column(name = "group_name", nullable = false, length = 100)
     private String groupName;
 
-    @Column(name = "ubuntu_gid", unique = true, nullable = false)
-    private Long ubuntuGid;
-
     @OneToOne(fetch = FetchType.LAZY)
-    @JoinColumn(name = "ubuntu_gid", referencedColumnName = "id_value", insertable = false, updatable = false)
+    @MapsId
+    @JoinColumn(name = "ubuntu_gid")
     private UsedId usedId;
 }
