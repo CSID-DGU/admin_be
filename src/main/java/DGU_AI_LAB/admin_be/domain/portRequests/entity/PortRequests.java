692e20c67609f9234702bff028461bcba1d85e27
kwdahun <kwdahun@naver.com>
2025-08-31 16:08:52 +0900

    [feat] #118 Add port request table

---
+++ src/main/java/DGU_AI_LAB/admin_be/domain/portRequests/entity/PortRequests.java
@@ -0,0 +1,31 @@
+package DGU_AI_LAB.admin_be.domain.portRequests.entity;
+
+import DGU_AI_LAB.admin_be.domain.requests.entity.Request;
+import DGU_AI_LAB.admin_be.global.common.BaseTimeEntity;
+import jakarta.persistence.*;
+import lombok.*;
+
+@Entity
+@Table(name = "port_requests")
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@AllArgsConstructor
+@Builder
+public class PortRequests extends BaseTimeEntity {
+
+    @Id
+    @Column(name = "port_number", nullable = false)
+    private Integer portNumber;
+
+    @Column(name = "usage_purpose", nullable = false, length = 1000)
+    private String usagePurpose;
+
+    @Column(name = "is_active", nullable = false)
+    @Builder.Default
+    private Boolean isActive = true;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "request_id", nullable = false)
+    private Request request;
+
+}
