3b7ed79f325a7705981df8bfbca11d0c5fa17b14
kwdahun <kwdahun@naver.com>
2025-09-01 15:00:48 +0900

    [fix] #118 Add port range validation

--- src/main/java/DGU_AI_LAB/admin_be/domain/portRequests/entity/PortRequests.java
+++ src/main/java/DGU_AI_LAB/admin_be/domain/portRequests/entity/PortRequests.java
@@ -3,6 +3,8 @@ package DGU_AI_LAB.admin_be.domain.portRequests.entity;
 import DGU_AI_LAB.admin_be.domain.requests.entity.Request;
 import DGU_AI_LAB.admin_be.global.common.BaseTimeEntity;
 import jakarta.persistence.*;
+import jakarta.validation.constraints.Max;
+import jakarta.validation.constraints.Min;
 import lombok.*;
 
 @Entity
@@ -19,6 +21,8 @@ public class PortRequests extends BaseTimeEntity {
     private Long id;
 
     @Column(name = "port_number", nullable = false, unique = true)
+    @Min(1)
+    @Max(65535)
     private Integer portNumber;
 
     @Column(name = "usage_purpose", nullable = false, length = 1000)
