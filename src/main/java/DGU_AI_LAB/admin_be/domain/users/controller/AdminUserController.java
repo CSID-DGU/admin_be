e308a944e16c94917f8b844ff6d1bd52bbadc15c
saokiritoni <soeun.love.lee@gmail.com>
2025-09-02 23:44:16 +0900

    [docs] #121 관리자용 유저 API 명세서 작성

--- src/main/java/DGU_AI_LAB/admin_be/domain/users/controller/AdminUserController.java
+++ src/main/java/DGU_AI_LAB/admin_be/domain/users/controller/AdminUserController.java
@@ -1,10 +1,8 @@
 package DGU_AI_LAB.admin_be.domain.users.controller;
 
-import DGU_AI_LAB.admin_be.domain.users.dto.request.UserUpdateRequestDTO;
 import DGU_AI_LAB.admin_be.domain.users.service.AdminUserService;
 import DGU_AI_LAB.admin_be.domain.users.service.UserService;
 import DGU_AI_LAB.admin_be.global.common.SuccessResponse;
-import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -12,7 +10,7 @@ import org.springframework.web.bind.annotation.*;
 @RestController
 @RequiredArgsConstructor
 @RequestMapping("/api/admin/users")
-public class AdminUserController {
+public class AdminUserController implements DGU_AI_LAB.admin_be.domain.users.controller.AdminUserApi {
 
     private final AdminUserService adminUserService;
     private final UserService userService;
