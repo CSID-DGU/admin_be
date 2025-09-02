52f00c84f447a81e15f3f07df156d661421865d9
kwdahun <kwdahun@naver.com>
2025-09-02 21:47:25 +0900

    [fix] Modify unique constraints from PortRequests entity

--- src/main/java/DGU_AI_LAB/admin_be/domain/portRequests/entity/PortRequests.java
+++ src/main/java/DGU_AI_LAB/admin_be/domain/portRequests/entity/PortRequests.java
@@ -1,6 +1,7 @@
 package DGU_AI_LAB.admin_be.domain.portRequests.entity;
 
 import DGU_AI_LAB.admin_be.domain.requests.entity.Request;
+import DGU_AI_LAB.admin_be.domain.resourceGroups.entity.ResourceGroup;
 import DGU_AI_LAB.admin_be.global.common.BaseTimeEntity;
 import jakarta.persistence.*;
 import jakarta.validation.constraints.Max;
@@ -8,7 +9,9 @@ import jakarta.validation.constraints.Min;
 import lombok.*;
 
 @Entity
-@Table(name = "port_requests")
+@Table(name = "port_requests", uniqueConstraints = {
+    @UniqueConstraint(columnNames = {"port_number", "rsgroup_id"})
+})
 @Getter
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
 @AllArgsConstructor
@@ -20,7 +23,7 @@ public class PortRequests extends BaseTimeEntity {
     @Column(name = "port_request_id")
     private Long portRequestId;
 
-    @Column(name = "port_number", nullable = false, unique = true)
+    @Column(name = "port_number", nullable = false)
     @Min(1)
     @Max(65535)
     private Integer portNumber;
@@ -35,5 +38,9 @@ public class PortRequests extends BaseTimeEntity {
     @ManyToOne(fetch = FetchType.LAZY)
     @JoinColumn(name = "request_id", nullable = false)
     private Request request;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "rsgroup_id", nullable = false)
+    private ResourceGroup resourceGroup;
 
 }
