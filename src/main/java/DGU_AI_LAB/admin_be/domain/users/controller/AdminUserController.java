15edbf36fb7240d6254ae005645b8835079158f7
saokiritoni <soeun.love.lee@gmail.com>
2025-09-02 23:40:40 +0900

    [chore] #121 관리자용 유저 정보 수정 API 삭제

--- src/main/java/DGU_AI_LAB/admin_be/domain/users/controller/AdminUserController.java
+++ src/main/java/DGU_AI_LAB/admin_be/domain/users/controller/AdminUserController.java
@@ -27,11 +27,6 @@ public class AdminUserController {
         return SuccessResponse.ok(adminUserService.getAllUsers());
     }
 
-    @PutMapping("/{id}") // TODO: 관리자용 updateUser는 사용 여부 논의 필요
-    public ResponseEntity<SuccessResponse<?>> updateUser(@PathVariable Long id, @Valid @RequestBody UserUpdateRequestDTO request) {
-        return SuccessResponse.ok(adminUserService.updateUser(id, request));
-    }
-
     @DeleteMapping("/{id}")
     public ResponseEntity<SuccessResponse<?>> deleteUser(@PathVariable Long id) {
         adminUserService.deleteUser(id);
