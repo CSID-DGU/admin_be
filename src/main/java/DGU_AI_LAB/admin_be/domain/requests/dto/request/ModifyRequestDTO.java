5a37ec28daa23786da43bb0e373789f66b44ffed
saokiritoni <soeun.love.lee@gmail.com>
2025-09-04 16:28:49 +0900

    [feat] #121 사용 변경 신청 항목값 추가

--- src/main/java/DGU_AI_LAB/admin_be/domain/requests/dto/request/ModifyRequestDTO.java
+++ src/main/java/DGU_AI_LAB/admin_be/domain/requests/dto/request/ModifyRequestDTO.java
@@ -4,6 +4,7 @@ import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.Positive;
 
 import java.time.LocalDateTime;
+import java.util.Set;
 
 public record ModifyRequestDTO(
         @NotBlank(message = "변경 사유는 필수입니다.")
@@ -12,6 +13,13 @@ public record ModifyRequestDTO(
         @Positive(message = "볼륨 크기는 양수여야 합니다.")
         Long requestedVolumeSizeGiB,
 
-        LocalDateTime requestedExpiresAt
+        LocalDateTime requestedExpiresAt,
+
+        Set<Long> requestedGroupIds,
+
+        Integer requestedResourceGroupId,
+
+        Long requestedContainerImageId
+
 ) {
 }
