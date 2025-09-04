74aba15973578eca627884b0f65363544c88328d
saokiritoni <soeun.love.lee@gmail.com>
2025-09-04 16:27:17 +0900

    [feat] #121 ChangeType enum 이름 변경

--- src/main/java/DGU_AI_LAB/admin_be/domain/requests/entity/ChangeType.java
+++ src/main/java/DGU_AI_LAB/admin_be/domain/requests/entity/ChangeType.java
@@ -1,5 +1,9 @@
 package DGU_AI_LAB.admin_be.domain.requests.entity;
 
 public enum ChangeType {
-    VOLUME_SIZE, EXPIRES_AT, RSGROUP_ID, IMAGE_ID, GROUPS
+    VOLUME_SIZE,
+    EXPIRES_AT,
+    GROUP, // 사용자가 속한 그룹 변경
+    RESOURCE_GROUP, // 리소스 그룹 변경
+    CONTAINER_IMAGE // 도커 이미지 변경
 }
