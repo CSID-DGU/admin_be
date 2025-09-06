3cbfaaf40173166bca4184f5273c492a32443b0b
saokiritoni <soeun.love.lee@gmail.com>
2025-09-07 00:26:02 +0900

    [feat] #112 그룹 생성 컨트롤러, DTO 수정

--- src/main/java/DGU_AI_LAB/admin_be/domain/groups/dto/request/CreateGroupRequestDTO.java
+++ src/main/java/DGU_AI_LAB/admin_be/domain/groups/dto/request/CreateGroupRequestDTO.java
@@ -6,15 +6,16 @@ import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Positive;
 import lombok.Builder;
 
-@Schema(description = "1. 그룹 생성 요청 DTO")
+@Schema(description = "그룹 생성 요청 DTO")
 @Builder
 public record CreateGroupRequestDTO(
-        @Schema(description = "할당할 우분투 GID (Group ID)", example = "1001")
-        @NotNull @Positive
-        Long ubuntuGid,
 
         @Schema(description = "생성할 그룹명", example = "developers")
-        @NotBlank
-        String groupName
+        @NotBlank(message = "그룹명은 필수입니다.")
+        String groupName,
+
+        @Schema(description = "그룹에 추가할 우분투 사용자 이름", example = "user1")
+        @NotBlank(message = "우분투 사용자 이름은 필수입니다.")
+        String ubuntuUsername
 ) {
 }
