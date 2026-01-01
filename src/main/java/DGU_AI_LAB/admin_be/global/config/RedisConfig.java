b376144160cbbf053106b3cb702fc1d270a665f4
saokiritoni <soeun.love.lee@gmail.com>
2026-01-01 22:12:09 +0900

    [fix] #155 Redis Template

---
+++ src/main/java/DGU_AI_LAB/admin_be/global/config/RedisConfig.java
@@ -0,0 +1,22 @@
+package DGU_AI_LAB.admin_be.global.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.redis.connection.RedisConnectionFactory;
+import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.data.redis.serializer.Jackson2JsonRedisSerializer;
+import org.springframework.data.redis.serializer.StringRedisSerializer;
+
+@Configuration
+public class RedisConfig {
+    @Bean
+    public RedisTemplate<String, Object> redisTemplate(RedisConnectionFactory connectionFactory) {
+        RedisTemplate<String, Object> template = new RedisTemplate<>();
+        template.setConnectionFactory(connectionFactory);
+
+        // Key는 String, Value는 JSON 형태로 저장
+        template.setKeySerializer(new StringRedisSerializer());
+        template.setValueSerializer(new Jackson2JsonRedisSerializer<>(Object.class));
+        return template;
+    }
+}
