b4ccb7ff1f70e410284f8b18daa245361697aed5
saokiritoni <soeun.love.lee@gmail.com>
2026-01-14 16:16:53 +0900

    [refactor] #155 - users.list 캐싱

--- src/main/java/DGU_AI_LAB/admin_be/global/config/RedisConfig.java
+++ src/main/java/DGU_AI_LAB/admin_be/global/config/RedisConfig.java
@@ -4,19 +4,26 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.redis.connection.RedisConnectionFactory;
 import org.springframework.data.redis.core.RedisTemplate;
-import org.springframework.data.redis.serializer.Jackson2JsonRedisSerializer;
+import org.springframework.data.redis.serializer.GenericJackson2JsonRedisSerializer;
 import org.springframework.data.redis.serializer.StringRedisSerializer;
 
 @Configuration
 public class RedisConfig {
+
     @Bean
     public RedisTemplate<String, Object> redisTemplate(RedisConnectionFactory connectionFactory) {
         RedisTemplate<String, Object> template = new RedisTemplate<>();
         template.setConnectionFactory(connectionFactory);
 
-        // Key는 String, Value는 JSON 형태로 저장
+        // Key는 String으로
         template.setKeySerializer(new StringRedisSerializer());
-        template.setValueSerializer(new Jackson2JsonRedisSerializer<>(Object.class));
+
+        // Value는 GenericJackson2JsonRedisSerializer 사용 (Class Type 정보 포함)
+        template.setValueSerializer(new GenericJackson2JsonRedisSerializer());
+
+        template.setHashKeySerializer(new StringRedisSerializer());
+        template.setHashValueSerializer(new GenericJackson2JsonRedisSerializer());
+
         return template;
     }
 }
