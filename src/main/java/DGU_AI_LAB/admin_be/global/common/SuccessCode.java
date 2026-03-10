16c354f83d96aca7346c08d54a5e999fa5823777
dongmin0204 <nm2205@dgu.ac.kr>
2026-03-11 00:58:03 +0900

    [fix] #190 SuccessCode enum 불필요한 세미콜론 제거

--- src/main/java/DGU_AI_LAB/admin_be/global/common/SuccessCode.java
+++ src/main/java/DGU_AI_LAB/admin_be/global/common/SuccessCode.java
@@ -25,7 +25,6 @@ public enum SuccessCode {
     EMAIL_SENT(HttpStatus.OK, "이메일 인증번호 전송 완료"),
     EMAIL_VERIFIED(HttpStatus.OK, "이메일 인증번호 인증 완료");
 
-    ;
     private final HttpStatus httpStatus;
     private final String message;
 }
